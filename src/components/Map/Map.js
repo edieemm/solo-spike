8bf1af6ca6d9c1de03c682d1e05acfb62ad88e21
Edith Emmings <emmings.edith@gmail.com>
2019-10-13 13:53:05 -0500

    marker working

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import { compose, withProps, lifecycle } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel";
-import { FormControl, Select, MenuItem, FormHelperText} from '@material-ui/core';
 
 const MyMapComponent = compose(
     withProps({
-        googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=place&key=${process.env.REACT_APP_MAP}`,
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=place&key=${process.env.API_KEY}`,
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
@@ -20,14 +19,17 @@ const MyMapComponent = compose(
         defaultZoom={12}
         defaultCenter={{ lat: 44.952228, lng: -93.2791737 }}
     >
-        {props.isMarkerShown &&
-            <MarkerWithLabel
-                position={{ lat: 44.952228, lng: -93.2791737 }}
-                // labelAnchor={new google.maps.Point(0, 0)}
-                labelStyle={{ backgroundColor: "yellow", fontSize: "32px", padding: "16px" }}
+        {/* {props.coords */}
+        {true  ?
+            <Marker 
+                // position={{ lat: props.coords.lat, lng: props.coords.lng }}
+                position={{ lat: 44.952228, lng: -93.2791737}}
+                label='Simpson Housing Services'
+                labelAnchor={new window.google.maps.Point(0, 0)}
+                labelStyle={{ backgroundColor: "yellow", fontSize: "14px", padding: "16px" }}
             >
-                <div>Simpson Housing Services</div>
-            </MarkerWithLabel>}
+            </Marker > 
+        : ''}
     </GoogleMap>
 )
 
--- src/components/MapForm/MapForm.js
+++ src/components/MapForm/MapForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Map from '../Map/Map'
 import { FormControl, Select, MenuItem, FormHelperText } from '@material-ui/core';
+import axios from 'axios'
 
 class App extends Component {
     state = {
@@ -8,14 +9,35 @@ class App extends Component {
             name: 'Simpson Housing Shelter',
             location: '2100 Pillsbury Avenue South, Minneapolis, MN 55404'
         }],
-        selected: {}
+        selected: {},
+        googlePlace: {}
     }
     handleChange = (event) => {
         this.setState({
             ...this.state,
             selected: event.target.value
         })
-        console.log('you chose', event.target.value)
+        console.log('you chose', event.target.value.location)
+        this.getSelection(event.target.value.location);
+    }
+    getSelection = (location) => {
+        axios({
+            method: 'GET',
+            url: `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${process.env.API_KEY}&fields=photos,formatted_address,name,rating,opening_hours,geometry&inputtype=textquery&input=${location}`,
+            proxyurl: "https://cors-anywhere.herokuapp.com/"
+        })
+        .then((response) => {
+            console.log(response.data)
+            this.setState({
+                ...this.state,
+                googlePlace: response.data.candidates[0]
+            })
+        }).catch(error => {
+            console.log('error finding place: ', error)
+        })
+    }
+    componentDidMount() {
+        // 
     }
     render() {
         return (
@@ -39,8 +61,10 @@ class App extends Component {
                 </Select>
                 <FormHelperText>Select Shelter</FormHelperText>
             </FormControl>
-
-            <Map selectedShelter={this.state.selected}/>
+            <br/>
+                {this.state.googlePlace.geometry ? JSON.stringify(this.state.googlePlace.geometry.location) : ''}
+            <br/>
+                <Map coords={this.state.googlePlace.geometry}/>
         </div>
     )};
 }
